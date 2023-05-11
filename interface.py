5e0abf0484d35321525d46fe993a932a96e2c5f7
surbhitt <surbhit.thakur@gmail.com>
2023-05-11 17:02:34 +0530

    adding interface and main

---
+++ interface.py
@@ -0,0 +1,73 @@
+import curses
+import brainpoke 
+
+def cmd_view(screen):
+	header = """
+	Th1s i$ hte brainpoke program
+	implement@tion of the brainf**k in python
+
+	=========================================
+	| > | move the pntr to rt mem add       |
+	| < | move the pntr to lft mem add      |
+	| + | inc cur mem add                   |
+	| - | dec cur mem add                   |
+	| . | out cur mem add                   |
+	| , | in cur mem add                    |
+	| [ | move to ], if cur mem add holds 0 |
+	| ] | move to [                         |   
+	=========================================
+	"""
+	screen.addstr(1, 1, header)
+
+def memory_map():
+	pass
+
+"""
+def handle_commands(op: chr):
+	intro2 = '\ton ? prompt enter the command \n\tto exit enter $$\n'
+	print(intro2)
+	while operation != "$$":
+		operation = input("\n\t? ")
+	
+		array, pointer, loop_pointer = operations_on_array(operation, array, pointer, loop_pointer)
+"""
+# begin_x = 20 ; begin_y = 7
+# height = 5 ; width = 40
+# win = curses.newwin(height, width, begin_y, begin_x)
+
+
+def interface():
+	memory = brainpoke.Memory()
+	mode = ord('c')
+	screen = curses.initscr()
+	curses.noecho()
+
+	while True:
+		screen.clear()
+		if mode == ord('c'):
+			cmd_view(screen)
+		elif mode == ord('m'):
+			memory_map(screen)
+		
+		screen.addstr(20, 1, '	? ' )	
+		user_input = screen.getstr()
+		if user_input == ord('c'):
+			if mode == ord('c'):
+				screen.addstr(50, 1, "already cmd mode")
+			else:
+				mode = ord('ord')
+		 
+		elif user_input == ord('m'):
+			if mode == ord('m'):
+				screen.addstr(50, 1, "already memory map mode")
+			else:
+				mode = ord('m')
+		elif user_input == ord('q'):
+			curses.endwin()
+			return 0
+		else:
+			memory.handle_operations(user_input)
+	
+		screen.refresh()
+	curses.nocbrea()
+	curses.endwin()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 #!/usr/bin/env python3
+from interface import interface
 
 """
 		to do
@@ -9,83 +10,10 @@
 	4. 
 """
 
-class Memory(object):
-	
-	def __init__(self):
-		self.memory = [0]*3000;
-		self.pointer = 0
-
-	def viewstate(self):
-    	print(self.memory)
-
-
-def menu ():
-    Intro_prompt = """
-    Th1s i$ hte brainpoke program
-    implement@tion of the brainf**k in python
-
-	=========================================
-	| > | move the pntr to rt mem add       |
-	| < | move the pntr to lft mem add      |
-	| + | inc cur mem add                   |
-	| - | dec cur mem add                   |
-	| . | out cur mem add                   |
-	| , | in cur mem add                    |
-	| [ | move to ], if cur mem add holds 0 |
-	| ] | move to [                         |   
-	=========================================
-    """
-    print(Intro_prompt)
-
-def handle_commands(op: chr):
-	
-
-def operations_on_array (operation, array: list, pointer: int, loop_pointer: list):
-    if operation == ">":
-        pointer += 1
-    elif operation == "<":
-        pointer -= 1
-    elif operation == "+":
-        array[pointer] += 1
-    elif operation == "-":
-        if array[pointer] - 1 > -1:
-            array[pointer] -= 1
-    elif operation == ",":
-        try:
-            inp = input("\tINPUT CHARACTER __ ")
-            array[pointer] = ord(inp)
-        except TypeError:
-            print("\tenter only a single character")
-    elif operation == ".":
-        if array[pointer] != 0:
-            print(f"\t  {chr(array[pointer])=}   is the char stored at {pointer=} ")
-        else:
-            print(f"\t  0   is the char stored at {pointer=} ")      
-    elif operation == "[":
-        if loop_pointer[0] == -1:
-            loop_pointer[0] = pointer
-        if array[pointer] == 0:
-            pointer = loop_pointer[1]
-            loop_pointer = [-1, -1]
-    elif operation == "]":
-        loop_pointer[1] = pointer
-        pointer = loop_pointer[0]
-    elif operation == "$$":
-        print("\tEXITING")
-        pass
-    else:
-        print("\tTHIS IS UNRECOGNISED COMMAND")
-    return array, pointer, loop_pointer
-
 
 def main ():
-    menu ()
-    
-    intro2 = "\ton ? prompt enter the command \n\tto exit enter $$\n"
-    print(intro2)
-    while operation != "$$":
-        operation = input("\n\t? ")
-        array, pointer, loop_pointer = operations_on_array(operation, array, pointer, loop_pointer)
+	interface()
+	
 
 if __name__ == '__main__':
-    main()
+	main()
